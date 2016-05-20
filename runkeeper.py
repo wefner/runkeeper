b464600b290ff4f56e7c32f9c5c3d865a43ad97a
wefner <fabregas.oriol@gmail.com>
2016-05-20 14:17:28 +0200

    Try/Excepts, activity details, small refactor

--- runkeeper.py
+++ runkeeper.py
@@ -7,7 +7,7 @@ import json
 from requests import Session
 from bs4 import BeautifulSoup as bfs
 from datetime import datetime
-from runkeeperExceptions import InvalidAuhentication, NoActivityInMonth
+from runkeeperExceptions import InvalidAuhentication, NoActivityInMonth, EndpointConnectionError, ProfileNotFound, InvalidActivityId, NoActivitiesFound
 
 email = ""
 password = keyring.get_password("runkeeper", email)
@@ -20,55 +20,118 @@ class Runkeeper(object):
         self.password = password
         self.session = Session()
         self.site = 'https://runkeeper.com'
-        self.__hidden_username = ''
+        self.__profile_username = ''
         self.__authenticate()
 
     def __authenticate(self):
+        """
+        Send all POST parameters and check for login validation cookie
+        :return: bool
+        """
         url = "{site}/login".format(site=self.site)
-        valid_authentication = self.session.post(url, data=self.__get_hidden_elements())
+        try:
+            valid_authentication = self.session.post(url, data=self.__get_hidden_elements())
+        except:
+            raise EndpointConnectionError
         if not valid_authentication.cookies.get('checker'):
             raise InvalidAuhentication
 
         return True
 
     def __get_hidden_elements(self):
+        """
+        Retrieve all POST parameters
+        :return: dict
+        """
         data = {'password': self.password, 'email': self.email}
         url = "{site}/login".format(site=self.site)
-        auth = self.session.post(url, data=data)
+        try:
+            hidden_session = self.session.post(url, data=data)
+        except:
+            raise EndpointConnectionError
 
-        soup = bfs(auth.text, "html.parser")
+        soup = bfs(hidden_session.text, "html.parser")
         form = soup.find_all('input', {'type': 'hidden'})
-        hidden_elements = { element.attrs['name'] : element.attrs['value'] for element in form}
+        hidden_elements = {element.attrs['name']: element.attrs['value'] for element in form}
         hidden_elements['email'] = self.email
         hidden_elements['password'] = self.password
 
         return hidden_elements
 
+    @property
+    def profile_username(self):
+        """
+        Get profile username or ID once logged in by using Session object
+        :return: str
+        """
+        url = "{site}/home".format(site=self.site)
+        try:
+            home = self.session.get(url)
+        except:
+            raise EndpointConnectionError
+
+        soup = bfs(home.text, "html.parser")
+        profile_url = soup.find('a', {'href': re.compile('/user/[a-zA-Z]|[0-9]/profile')})
+
+        self.__profile_username = profile_url.attrs['href'].split('/')[2]
+
+        if not self.__profile_username:
+            raise ProfileNotFound
+
+        return self.__profile_username
+
     def get_activities_month(self, month, year=None):
+        """
+        Get activities in specified month and year
+        :param month: str (month abbreviated)
+        :param year: str (YYYY)
+        :return: Activity object
+        """
+        activity_details = []
         year = year or str(datetime.today().year)
 
-        startDate = "{month}-01-{year}".format(month=month, year=year)
-        payload = {"userName": self.hidden_username, "startDate":startDate}
+        start_date = "{month}-01-{year}".format(month=month, year=year)
+        payload = {"userName": self.profile_username, "startDate": start_date}
         url = "{site}/activitiesByDateRange".format(site=self.site)
-        request = self.session.get(url, params=payload)
-        activities = json.loads(request.text)['activities']
 
-        if not activities:
+        try:
+            activities_month_request = self.session.get(url, params=payload)
+        except:
+            raise EndpointConnectionError
+
+        try:
+            activities_month = json.loads(activities_month_request.text)['activities']
+        except:
+            raise NoActivitiesFound
+
+        if not activities_month:
             raise NoActivityInMonth
 
-        return [Activity(activity) for activity in activities[year][month]]
+        for activity in activities_month[year][month]:
+            activity['details'] = self.get_activity_details(activity['activity_id'])
+            activity_details.append(activity)
 
+        return [Activity(activity) for activity in activity_details]
 
-    @property
-    def hidden_username(self):
-        url = "{site}/home".format(site=self.site)
-        auth = self.session.get(url)
-        soup = bfs(auth.text, "html.parser")
-        h_user = soup.find('a', {'href':re.compile('/user/[a-zA-Z]|[0-9]/profile')})
-        self.__hidden_username = h_user.attrs['href'].split('/')[2]
+    def get_activity_details(self, activity_id):
+        """
+        Returns other useful information from a particular activity
+        :param activity_id: String
+        :return: JSON Object
+        """
+        url = "{site}/ajax/pointData".format(site=self.site)
+        activity_params = {"activityId": activity_id}
+        try:
+            activity_request = self.session.get(url, params=activity_params)
+        except:
+            raise EndpointConnectionError
 
-        return self.__hidden_username
+        try:
+            activity_details = json.loads(activity_request.text)
+        except:
+            raise InvalidActivityId
 
+        return activity_details
 
 class Activity(object):
     def __init__(self, info):
@@ -83,9 +146,13 @@ class Activity(object):
         self.caption = ''
         self.activity_type = ''
         self.date = ''
+        self.calories = ''
         self._parse()
 
     def _parse(self):
+        """
+        Stores activity value as object from dictionary key in a variable
+        """
         self.date = datetime(int(self.info.get('year')),
                              int(self.info.get('monthNum')),
                              int(self.info.get('dayOfMonth')))
@@ -97,6 +164,7 @@ class Activity(object):
         self.live = self.info.get('live')
         self.caption = self.info.get('mainText')
         self.activity_type = self.info.get('type')
+        self.calories = self.info.get('details', {}).get('statsCalories')
 
 if __name__ == '__main__':
     runkeeper = Runkeeper(email, password)
@@ -112,7 +180,5 @@ if __name__ == '__main__':
         print "Live Activity: {live}".format(live=activity.live)
         print "Caption: {caption}".format(caption=activity.caption)
         print "Activity Type: {activity_type}".format(activity_type=activity.activity_type)
+        print "Calories Burned: {calories}".format(calories=activity.calories)
         print ""
-
-
-
--- runkeeperExceptions.py
+++ runkeeperExceptions.py
@@ -7,3 +7,19 @@ class InvalidAuhentication:
 class NoActivityInMonth:
     def __init__(self):
         pass
+
+class EndpointConnectionError:
+    def __init__(self):
+        pass
+
+class ProfileNotFound:
+    def __init__(self):
+        pass
+
+class InvalidActivityId:
+    def __init__(self):
+        pass
+
+class NoActivitiesFound:
+    def __init__(self):
+        pass
